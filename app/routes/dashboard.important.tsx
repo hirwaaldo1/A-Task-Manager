c03b52270cba01efe9d91f003b6bb1199302b3bf
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-04-22 21:36:02 +0200

    feat: add importance page

---
+++ app/routes/dashboard.important.tsx
@@ -0,0 +1,28 @@
+import { useOutletContext } from "@remix-run/react";
+import { FiStar } from "react-icons/fi";
+import Empty from "~/components/error/Empty";
+import TaskCard from "~/components/ui/TaskCard";
+
+export default function Important() {
+  const { allTask }: any = useOutletContext();
+  const tasks = allTask.filter((task: any) => task.isImportance);
+  return (
+    <div className="bg-[#1c1c1c] text-white w-[80%] py-10 px-12 relative">
+      <div className="flex items-center gap-2 mb-5">
+        <FiStar size={20} className="fill-[#f5b6c2] text-[#f5b6c2]" />
+        <span className="text-[#f5b6c2] text-2xl font-semibold">Important</span>
+      </div>
+      {tasks.length === 0 ? (
+        <Empty />
+      ) : (
+        <div className="overflow-auto">
+          <div className="flex flex-col gap-1 w-full">
+            {tasks.map((task: any) => {
+              return <TaskCard key={task.id} task={task} />;
+            })}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
